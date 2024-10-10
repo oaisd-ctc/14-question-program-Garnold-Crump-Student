db82adbbab6ec819d7c5a13b43d3c19a15a7c5a7
Garnold-Crump-Student <gacr846@student.oaisd.org>
2024-10-10 13:38:58 +0000

    main

--- Program.cs
+++ Program.cs
@@ -9,57 +9,60 @@ namespace Questions
 
       Console.WriteLine("What is your name");
       string firstName = Console.ReadLine();
-      Console.WriteLine("Your name is " + (firstName));
+
 
 
       Console.WriteLine("Enter your age:");
       int age = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("Your age is: " + age);
+
 
       Console.WriteLine("What is your eye color?");
       string eyeColor = Console.ReadLine();
-      Console.WriteLine("Your eye color is " + (eyeColor));
+
+
+      Console.WriteLine("What is your hair color?");
+      string hairColor = Console.ReadLine();
 
       Console.WriteLine("What is your shoe size");
       int shoeSize = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("Your shoe size is " + (shoeSize));
+
 
       Console.WriteLine("What is your favorite color?");
       string favoriteColor = Console.ReadLine();
-      Console.WriteLine("Your favorite color is " + (favoriteColor));
+
 
       Console.WriteLine("What is your favorite tv show?");
       string tvShow = Console.ReadLine();
-      Console.WriteLine("Your tv show is " + (tvShow));
+
 
       Console.WriteLine("who is your favorite teacher?");
       string favTeacher = Console.ReadLine();
-      Console.WriteLine("your favorite teacher is " + (favTeacher));
+
 
       Console.WriteLine("what is your favorite class?");
       string favClass = Console.ReadLine();
-      Console.WriteLine("your favorite class is " + (favClass));
+
 
       Console.WriteLine("what is your favorite holiday?");
       string favHoliday = Console.ReadLine();
-      Console.WriteLine("your favorite holiday is " + (favHoliday));
+
 
       Console.WriteLine("what is your favorite season?");
       string favSeason = Console.ReadLine();
-      Console.WriteLine("your favorite season is " + (favSeason));
 
-         Console.WriteLine("what is your dream job?");
+
+      Console.WriteLine("what is your dream job?");
       string dreamJob = Console.ReadLine();
-      Console.WriteLine("your dream job is " + (dreamJob));
 
-      Console.WriteLine("your age in 5 years is");
-       Console.WriteLine(" " + (age + 5));
 
-         Console.WriteLine("How many siblings do you have");
+
+
+      Console.WriteLine("How many siblings do you have");
       string siblings = Console.ReadLine();
-      Console.WriteLine("you have " + (siblings)+ " Siblings");
 
 
+      Console.WriteLine("my friends name is " + (firstName) + " they are " + (age) + " and in 5 years he is " + (age + 5) + " there Eye color is " + (eyeColor) +
+      " there hair color is " + (hairColor) + " there shoe size is " + (shoeSize) + " there favorite color is " + (favoriteColor) + " there favorite tv show is " + (tvShow) + " there favorite teacher is " + (favTeacher) + " there favorite class is " + (favClass) + " there holiday is " + (favHoliday) + " there dream job is " + (dreamJob) + " they have " + (siblings) + " siblings ");
 
 
 
